e565f27d481caf2fdcd1d6b2cac5c0d215742bcc
Clint Sellen <12685400@student.uts.edu.au>
2023-04-03 19:18:39 +1000

    added backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "awsamplify839bf793": {
       "AppClientID": "string",
